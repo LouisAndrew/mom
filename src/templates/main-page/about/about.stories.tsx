c1d212ac87783f6a4cdc9e7dcb896b601062bc03
Louis Andrew <louisandrew3@gmail.com>
2020-08-08 01:12:55 +0200

    ☑ Updated about section

--- src/templates/main-page/about/about.stories.tsx
+++ src/templates/main-page/about/about.stories.tsx
@@ -7,7 +7,7 @@ const story = storiesOf('Main Page.About', module);
 
 story.add('Component', () => {
     const src =
-        'https://res.cloudinary.com/dsvdffre0/image/upload/v1596016031/github-icon.png';
-    const imgAlt = 'github-icon';
+        'https://res.cloudinary.com/dsvdffre0/image/upload/v1596841161/stefan-stefancik-QXevDflbl8A-unsplash_ei8bvp.jpg';
+    const imgAlt = 'smiling-woman';
     return <About imgSrc={src} imgAlt={imgAlt} />;
 });
--- src/templates/main-page/about/about.tsx
+++ src/templates/main-page/about/about.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import { color, layout, space, flexbox, ColorProps } from 'styled-system';
+import {
+    color,
+    layout,
+    space,
+    flexbox,
+    border,
+    boxShadow,
+    ColorProps,
+    BorderProps,
+    BoxShadowProps,
+} from 'styled-system';
 import Img, { FluidObject } from 'gatsby-image';
 
-import { OuterWrapper, H2, P, PositioningProps } from 'styles';
+import { OuterWrapper, H2, P, PositioningProps, theme } from 'styles';
 
 type Props = {
     imgAlt: string;
@@ -14,6 +24,11 @@ type Props = {
 type ContainerProps = ColorProps & PositioningProps & {};
 
 type WrapperProps = PositioningProps & {};
+
+type DetailsWrapperProps = ColorProps &
+    PositioningProps &
+    BorderProps &
+    BoxShadowProps & {};
 
 const Container: React.FC<ContainerProps> = styled.div<ContainerProps>`
     display: grid;
@@ -29,6 +44,16 @@ const Wrapper: React.FC<WrapperProps> = styled.div<WrapperProps>`
     ${flexbox}
 `;
 
+const DetailsWrapper: React.FC<DetailsWrapperProps> = styled.div<
+    DetailsWrapperProps
+>`
+    ${color}
+    ${space}
+    ${border}
+    ${boxShadow}
+    ${layout}
+`;
+
 /**
  * Providing imgSrc to get the flexibility to develop in storybook
  * On the actual project: gotta be using the imgFluidSrc and get the text contents from gatsby
@@ -40,14 +65,28 @@ const About: React.FC<Props> = ({ imgAlt, imgSrc, imgFluidSrc }) => {
         'Quis ut sunt labore commodo amet velit laborum eu laboris. Lorem officia consectetur reprehenderit in ut ad anim cillum. Aute proident quis consequat nisi nulla non. Est ex minim aute qui commodo. Reprehenderit exercitation consectetur tempor aliquip nulla tempor cillum. Qui sint reprehenderit in anim aliqua labore veniam magna enim.';
 
     return (
-        <Container bg="dark.0" py={[6, 6, 7, 7]}>
+        <Container bg="bg" py={[6, 6, 7, 7]}>
             <OuterWrapper display="flex" justifyContent="center">
                 <Wrapper
-                    width={4 / 5}
+                    width={[0.9]}
                     display="flex"
                     flexDirection="column"
                     alignItems="center"
                     justifyContent="center"
+                    css={`
+                        & > img {
+                            border-radius: 8px;
+                            width: 80%;
+                            transform: translateY(24px);
+
+                            @media screen and (min-width: ${theme
+                                    .breakpoints[1]}) {
+                                width: 40%;
+                                align-self: flex-start;
+                                transform: translate(32px, 32px);
+                            }
+                        }
+                    `}
                 >
                     {imgFluidSrc ? (
                         <Img fluid={imgFluidSrc} alt={imgAlt} />
@@ -55,12 +94,26 @@ const About: React.FC<Props> = ({ imgAlt, imgSrc, imgFluidSrc }) => {
                         <img src={imgSrc} alt={imgAlt} />
                     )}
 
-                    <H2 color="light.0" textAlign="center" py={[3, 3, 4, 4]}>
-                        {headingText}
-                    </H2>
-                    <P color="light.0" textAlign="center">
-                        {bodyText}
-                    </P>
+                    <DetailsWrapper
+                        bg="light.0"
+                        py={[6, 7]}
+                        pr={[3, 5, 6]}
+                        pl={[3, 5, 9]}
+                    >
+                        <H2
+                            color="dark.0"
+                            textAlign={['center', 'center', 'left']}
+                        >
+                            {headingText}
+                        </H2>
+                        <P
+                            color="dark.0"
+                            textAlign={['center', 'center', 'left']}
+                            py={[4]}
+                        >
+                            {bodyText}
+                        </P>
+                    </DetailsWrapper>
                 </Wrapper>
             </OuterWrapper>
         </Container>
